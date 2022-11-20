fdf8a43c7939d5282f2555b6726c8d46c17683f3
Karthick47v2 <Karthicksharma11@gmail.com>
2022-11-20 13:38:00 +0530

    add html heading to markdown

--- converter.py
+++ converter.py
@@ -1,19 +1,30 @@
-which_to_which = input("Do you want to convert - \n 1.HTML to Markdown\n2.Markdown to HTML\n\nEnter 1 or 2: ")
+import re
 
+INUPT_FILE_NAME = "input.txt"
+OUTPUT_FILE_NAME = "output.txt"
+
+which_to_which = input(
+    "Do you want to convert - \n 1.HTML to Markdown\n2.Markdown to HTML\n\nEnter 1 or 2: ")
+
+
+def custom_make_translation(txt, trans):
+    # convert multiple string easily with dictionary
+    regex = re.compile('|'.join(map(re.escape, trans)))
+    return regex.sub(lambda match: trans[match.group(0)], txt)
 
 
 def htmltomd(source_code):
     # HEADINGS
-    # <h1> to #
-    md_code = source_code.replace("<h1", "#")
-    md_code = md_code.replace("</h1>", "\n") # Remember to put md_code.replace() and not source_code.replace() to continue making changes to the modified code.
-    md_code = md_code.replace(">", " ")
+    # <h1-6> to # - ######
+
+    md_code = custom_make_translation(
+        source_code, {f'<h{i}>': '#'*i + ' ' for i in range(1, 7)})
+    md_code = custom_make_translation(
+        md_code, {f'</h{i}>': '\n' for i in range(1, 7)})
 
     # LINKS
 
-
     return md_code
-
 
 
 def mdtohtml(source_code):
